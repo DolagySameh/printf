5a592a17b79a44c368189aa5597661ace2f4b343
DolagySameh <DolagySameh1@example.com>
2023-09-19 18:52:08 -0700

    commit printf moduls function

---
+++ printf_moduls.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+* printf_moduls - print moduls
+* Description: print moduls
+* @op: pointer to first arg
+* Return: length
+*/
+int printf_moduls(va_list op)
+{
+	_putchar('%');
+	(void)op;
+	return (1);
+}
